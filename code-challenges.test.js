de472f0d8d5ff0b979e06736be2402e1526da5c7
AJCodes1 <etsncamacho@gmail.com>
2023-06-12 17:12:27 -0700

    week 5 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions with Jest
 
+// const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,12 +16,45 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+describe("codedMessage", () => {
+    it("takes in a string and returns a string with a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0", () => {
+        const secretCodeWord1 = "Lackadaisical"
+        // Expected output: "L4ck4d41s1c4l"
+        const secretCodeWord2 = "Gobbledygook"
+        // Expected output: "G0bbl3dyg00k"
+        const secretCodeWord3 = "Eccentric"
+        // Expected output: "3cc3ntr1c" 
+      expect(codedMessage(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(codedMessage(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+      expect(codedMessage(secretCodeWord3)).toEqual("3cc3ntr1c")
+      
+    })
+  }) 
+
+  const codedMessage = (str) => {                    //create a function called codedMessage that takes in a string 
+    let newStr = ""                                  // define a new empty string 
+    for(let i = 0; i < str.length; i++) {            // create a for loop checking length of string 
+        if(str[i] === "a" || str[i] === "A") {          //if string is equal to "a" or "A" add the number 4 to newStr
+            newStr += "4"
+        }
+        else if(str[i] === "e" || str[i] === "E") {     //else if str is equal to "e" or "E" add the number 3 to newStr
+            newStr += "3"
+        }
+        else if(str[i] === "i" || str[i] === "I") {     //else if str is equal to "i" or "I" add the number 1 to newStr
+            newStr += "1"
+        }
+        else if(str[i] === "o" || str[i] === "O") {     //else if str is equal to "o" or "O" add the number 0 to newStr
+            newStr += "0"
+        }
+        else {
+            newStr += str[i]                               //else store current value to the newStr
+        }
+    }
+    return newStr
+
+  }
+
+
 
 // b) Create the function that makes the test pass.
 
@@ -29,12 +64,32 @@ const secretCodeWord3 = "Eccentric"
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe("findWords", () => {
+    it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter", () => {
+        const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+        
+        const filterLetterA = "a"
+        // Expected output: ["Mango", "Apricot", "Peach"]
+        const filterLetterE = "e"
+        // Expected output: ["Cherry", "Blueberry", "Peach"]
+        
+        expect(findWords(fruitArray, filterLetterA)).toEqual(["Mango", "Apricot", "Peach"])
+        expect(findWords(fruitArray, filterLetterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+        
+    })
+}) 
 
-const filterLetterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const filterLetterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+const findWords = (words, letter) => {              // create a function that takes in an array and a letter as parameters
+    const filteredWords = []                        // create an empty array to push into
+    for(let i = 0; i < words.length; i++) {         // create for loop
+        if(words[i].includes(letter)) {             // check if current word includes the letter
+            filteredWords.push(words[i])            // push word into new array 
+        }
+    }
+    return filteredWords
+}
+ 
+
 
 // b) Create the function that makes the test pass.
 
@@ -43,15 +98,53 @@ const filterLetterE = "e"
 // Pseudo code:
 
 // a) Create a test with expect statements using the variable provided.
+describe("isFullHouse", () => {
+    it("takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
+        const hand1 = [5, 5, 5, 3, 3]
+        // Expected output: true
+        const hand2 = [5, 5, 3, 3, 4]
+        // Expected output: false
+        const hand3 = [5, 5, 5, 5, 4]
+        // Expected output: false
+        const hand4 = [7, 2, 7, 2, 7]
+        // Expected output: true
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
+        expect(isFullHouse(hand1)).toEqual(true)
+        expect(isFullHouse(hand2)).toEqual(false)
+        expect(isFullHouse(hand3)).toEqual(false)
+        expect(isFullHouse(hand4)).toEqual(true)
+    })
+})
+
+const isFullHouse = (numbers) => {      // create a function called isFullHouse that takes in numbers
+    if(numbers.length !== 5) {          // if length of number is not 5 return false
+        return false 
+    }
+
+    const counts = {}                            //create an object counts to keep track of numbers 
+    for(let i = 0; i < numbers.length; i++) {    //iterate 
+        const number = numbers[i]       
+        if(counts[number]) {                     // if counts contain the current number 
+            counts[number]++                     // increment the count by 1 if its already counted 
+        }
+        else {                                  // start counting from 1 if its a new number
+            counts[number] = 1
+        }
+    }
+
+    const values = Object.values(counts)        //get the counts of each number
+    
+    if(values.length !==2) {                     //check if there are exactky 2 distinct numbers 
+        return false
+    }
+
+    const hasThreeOfKind = values.includes(3)   //check of there are 3of a kind and a pair
+    const hasTwoOfKind = values.includes(2)
+
+    return hasThreeOfKind && hasTwoOfKind       
+
+}
+ 
 
 // b) Create the function that makes the test pass.
 
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,49 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a Ruby class?
 
-Your answer:
+Your answer: calling super in a ruby class passes the arguments of the child method to the parent method.
 
-Researched answer:
+Researched answer:The super keyword refers to superclass (parent) objects. It is used to call superclass methods, and to access the superclass constructor.
 
 2. What is a gem?
 
-Your answer:
+Your answer: a gem is a bunch of code that gives you access to custom methods someone else wrote.
 
-Researched answer:
+Researched answer: a gem is library or snippet of code functionality. Gems are managed by the Ruby package manager called RubyGems.
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: is a collection of data items with pre-defined relationships between them, these items are organized as a set of tables with columns and rows.
 
-Researched answer:
+Researched answer: is almost exactly like an Excel spreadsheet or Google Sheets document with information displayed in rows and columns(MySQL, PostgreSQL, MariaDB, Microsoft SQL server, Oracle Database)
 
 4. What are primary keys? Why purpose does a primary key serve?
 
-Your answer:
+Your answer: a primary key contains values and identifies each row in a table, and their purpose is to extract data from a table.
 
-Researched answer:
+Researched answer: In rails every row of a table has a primary key named "id" and that ID is guaranteed to be unique, this prevents problems like 2 intances being confused for each other because they have the same name.
 
 5. What are the five HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: post, get, put, patch, delete.
+CRUD: create, read, update, delete
 
-Researched answer:
+Researched answer: 5 verves of HTTP are post, get, put, patch, abd delete.
+CRUD: retrieve a list of items(GET)
+Create and item(POST)
+Update an item(PUT)
+Delete an item(DELETE)
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Model validations:
+1. Model validations: are the best way to ensure tha only valid data is saved into your database
 
-2. RESTful routes:
+2. RESTful routes: is a route that improves a mapping of HTTP verbs to the CRUD actions that are defined in our controllers
 
-3. ERB:
+3. ERB: ERB files contain source code written in a programming language of the same name.
 
-4. Params:
+4. Params: variables that are defined in method definition and which represent the ability of a method to accept arguments.
 
-5. API:
+5. API: application programming interface is a way for one system to communicate with other external systems 
