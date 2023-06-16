72d9e6e1f8e6c6eb18a3cd376c5d630159bdcc4e
AJCodes1 <etsncamacho@gmail.com>
2023-06-16 12:50:09 -0700

    2nd ruby question finished

--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -1,29 +1,29 @@
-# ASSESSMENT 5: Ruby Coding Practical Questions
-# MINASWAN
+# # ASSESSMENT 5: Ruby Coding Practical Questions
+# # MINASWAN
 
-# --------------------1) Create a method that takes in an array of words and a single letter and returns an array of all the words containing that particular letter. Use the test variables provided.
+# # --------------------1) Create a method that takes in an array of words and a single letter and returns an array of all the words containing that particular letter. Use the test variables provided.
 
-beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
+# beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
 
-filter_letter_o = 'o'
-# Expected output: ['coffee', 'soda water']
-filter_letter_t = 't'
-# Expected output: ['tea', 'water', 'soda water']
+# filter_letter_o = 'o'
+# # Expected output: ['coffee', 'soda water']
+# filter_letter_t = 't'
+# # Expected output: ['tea', 'water', 'soda water']
 
 
-def find_words(words, letter)           
-filtered_words = []                     
+# def find_words(words, letter)           
+# filtered_words = []                     
 
-words.each do |word|                   
-    if word.include?(letter)          
-        filtered_words << word         
-    end
-end
+# words.each do |word|                   
+#     if word.include?(letter)          
+#         filtered_words << word         
+#     end
+# end
 
-return filtered_words
-end
+# return filtered_words
+# end
 
-puts find_words
+# puts find_words
 
 
 
@@ -33,61 +33,68 @@ puts find_words
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+def sorted_arrays(hash)
+   combined_arrays = hash.values.flatten
+   combined_arrays.sort
+end
+puts sorted_arrays(us_states)
+
+
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
-class Bike 
-    attr_reader :model, :wheels, :current_speed
+# class Bike 
+#     attr_reader :model, :wheels, :current_speed
 
-    def initialize(model, wheels = 2)
-        @model = model
-        @wheels = wheels
-        @current_speed = 0
-    end
+#     def initialize(model, wheels = 2)
+#         @model = model
+#         @wheels = wheels
+#         @current_speed = 0
+#     end
 
-    def bike_info
-        "The #{@model} has #{@wheels} wheels and is going #{@current_speed} mph"
-    end
-end
-bike = Bike.new("Trek bike")
-puts bike.bike_info
-# Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
-# -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
+#     def bike_info
+#         "The #{@model} has #{@wheels} wheels and is going #{@current_speed} mph"
+#     end
+# end
+# bike = Bike.new("Trek bike")
+# puts bike.bike_info
+# # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+# # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
-class Bike 
-    attr_reader :model, :wheels, :current_speed
+# class Bike 
+#     attr_reader :model, :wheels, :current_speed
 
-    def initialize(model, wheels = 2)
-        @model = model
-        @wheels = wheels
-        @current_speed = 0
-    end
+#     def initialize(model, wheels = 2)
+#         @model = model
+#         @wheels = wheels
+#         @current_speed = 0
+#     end
 
-    def bike_info
-        "The #{@model} has #{@wheels} wheels and is going #{@current_speed} mph"
-    end
+#     def bike_info
+#         "The #{@model} has #{@wheels} wheels and is going #{@current_speed} mph"
+#     end
 
-    def pedal_faster(amount)
-        @current_speed += amount
-    end
+#     def pedal_faster(amount)
+#         @current_speed += amount
+#     end
 
-    def brake(amount)
-        @current_speed -= amount
-        @current_speed = 0 if @current_speed.negative?
-    end
-end
-bike = Bike.new("Trek bike")
-puts bike.bike_info
+#     def brake(amount)
+#         @current_speed -= amount
+#         @current_speed = 0 if @current_speed.negative?
+#     end
+# end
+# bike = Bike.new("Trek bike")
+# puts bike.bike_info
 
-bike.pedal_faster(10)
-puts bike.bike_info
+# bike.pedal_faster(10)
+# puts bike.bike_info
 
-bike.pedal_faster(18)
-puts bike.bike_info
+# bike.pedal_faster(18)
+# puts bike.bike_info
 
-bike.brake(5)
-puts bike.bike_info
+# bike.brake(5)
+# puts bike.bike_info
 
-bike.brake(25)
-puts bike.bike_info
+# bike.brake(25)
+# puts bike.bike_info
 
 
 # Expected output example: my_bike.pedal_faster(10) => 10
